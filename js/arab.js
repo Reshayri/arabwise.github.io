05aea40e7d7b45f2dc716a69bb289b3dc3cf2ea2
Reshayri <39553581+Reshayri@users.noreply.github.com>
2018-12-16 17:38:26 +0200

    Add files via upload

---
+++ js/arab.js
@@ -0,0 +1,121 @@
+// alert("Hallo!");
+//Begin script 
+function trans1 () {
+var message; 
+message= 'Sleep will not help, if your soul is tired. ';
+document.getElementById("demo1").innerHTML = message;
+
+//alert(message);
+}
+function trans2() {
+	var message;
+	message='Say, if your words are better than silence.';
+	document.getElementById("demo2").innerHTML = message;
+	//alert(message);
+}
+function trans3 () {
+	var message;
+	message=' The army of sheep led by a lion, will win an army of lions led by a sheep.';
+	document.getElementById("demo3").innerHTML = message;
+	//alert(message);
+}
+function trans4 () {
+	var message;
+	message=' Mistake denial is dual wrong. ';
+	document.getElementById("demo4").innerHTML = message;
+	//alert(message);
+}
+function trans5 () {
+	var message;
+	message= ' Wise woman adds sugar to the words to say to a man, and collect all the salt of the mans words.';
+	document.getElementById("demo5").innerHTML = message;
+	//alert(message);
+}
+function trans6 () {
+	var message;
+	message= 'The experience of a lifetime seven wise rules better. ';
+	document.getElementById("demo6").innerHTML = message;
+	//alert(message);
+}
+function trans7 () {
+	var message;
+	message= ' Tenderness and kindness can also keep the elephant on the hair. ';
+	document.getElementById("demo7").innerHTML = message;
+	//alert(message);
+}
+function trans8 () {
+	var message;
+	message= ' Winds are not as they want it ships. ';
+	document.getElementById("demo8").innerHTML = message;
+	//alert(message);
+}
+function trans9 () {
+	var message;
+	message= ' He who seeks a faultless friend, alone remain. ';
+	document.getElementById("demo9").innerHTML = message;
+	//alert(message);
+}
+
+function trans10 () {
+	var message;
+	message= 'Always sunny days in the desert to give birth. ';
+	document.getElementById("demo10").innerHTML = message;
+	//alert(message);
+}
+function trans11 () {
+	var message;
+	message= ' If souls even left a moldy siding, he always will be a singing bird. ';
+	document.getElementById("demo11").innerHTML = message;
+	//alert(message);
+}
+
+function trans12 () {
+	var message;
+	message= ' Overcoming pride, man becomes enjoyable. Overcoming anger, a person becomes happy. Overcoming the passion, man becomes successful. Overcoming greed a man becomes happy. ';
+	document.getElementById("demo12").innerHTML = message;
+	//alert(message);
+}
+function trans13 () {
+	var message;
+	message= ' Best conqueror is one who is able to overcome himself. ';
+	document.getElementById("demo13").innerHTML = message;
+	//alert(message);
+}
+function trans14 () {
+	var message;
+	message= ' If you are afraid, do not tell, if I say, do not be afraid. ';
+	document.getElementById("demo14").innerHTML = message;
+	//alert(message);
+}
+function trans15 () {
+	var message;
+	message= 'Do not open the door, which can not be closed.  ';
+	document.getElementById("demo15").innerHTML = message;
+	//alert(message);
+}
+function trans16 () {
+	var message;
+	message= ' Even a small stone to bring loved ones back home. ';
+	document.getElementById("demo16").innerHTML = message;
+	//alert(message);
+}
+function trans17 () {
+	var message;
+	message= ' In addition to taking care of your loved one will not share. ';
+	document.getElementById("demo17").innerHTML = message;
+	//alert(message);
+}
+function trans18 () {
+	var message;
+	message= ' It is better to be a little son crying, after you than for him. ';
+	document.getElementById("demo18").innerHTML = message;
+	//alert(message);
+}
+
+function trans19 () {
+	var message;
+	message= 'The bride to recognize by her mother.  ';
+	document.getElementById("demo19").innerHTML = message;
+	//alert(message);
+}
+//END script
